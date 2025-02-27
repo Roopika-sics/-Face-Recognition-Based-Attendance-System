522b0ddc1e4047154533a2e0e12883148f156c51
Aravind Premkumar <aravindpremkumar.sics@gmail.com>
2025-02-27 13:40:22 +0530

    landing page added

--- student/urls.py
+++ student/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('student_registration/',views.student_register,name='student_registration'),
-    path('student_login/',views.student_login,name='student_login')
+    path('student_login/',views.student_login,name='student_login'),
+    path('',views.student_landing_page,name='student_landing_page'),
 ]
